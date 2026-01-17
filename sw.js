a2682be3ac4eec29377e58d899b73056599e2dbb
wauto87-droid <wauto87@gmail.com>
2026-01-17 10:13:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Minimal Service Worker to enable "Add to Home Screen"
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('finance-flow-store').then((cache) => cache.addAll([
+      './index.html',
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
